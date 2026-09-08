c20f12359bf644ac5e572c8a7ef42eb1ddef6f7b
MiguelGavito <mgavitogzz@gmail.com>
2026-09-08 17:19:53 +0200

    added flake for lab 2

---
+++ lab2-AdversarialSearch/flake.nix
@@ -0,0 +1,22 @@
+
+{
+  description = "TDDC17 Lab 2";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "aarch64-darwin";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [
+          pkgs.python313
+        ];
+      };
+    };
+}
