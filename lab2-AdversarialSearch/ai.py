98c44d21d0616e19161bfb8f43777f2c3f947a7b
MiguelGavito <mgavitogzz@gmail.com>
2026-09-09 18:32:20 +0200

    did the minmax for the lab2, need check the algo

--- lab2-AdversarialSearch/ai.py
+++ lab2-AdversarialSearch/ai.py
@@ -18,7 +18,56 @@ class Random(AI):
 class MinMax(AI):
     @staticmethod
     def best_move(current_state: State, objective: Objective):
-        pass
+
+        # check my options
+        moves = current_state.available_moves()
+
+        best_move = None
+        best_value = None
+
+        for move in moves:
+            next_state = current_state.next_state(move)
+            value = MinMax.minmax(next_state)
+
+        if best_value is None:
+            best_value = value
+            best_move = move
+
+        elif objective.value == 0: # MAX
+            if value > best_value:
+                best_value = value
+                best_move = move
+        else: # MIN
+            if value < best_value:
+                best_value = value
+                best_move = move
+
+        
+        return best_move
+
+    @staticmethod
+    def minmax(state: State):
+        moves = state.available_moves()
+
+        
+        if not moves:
+            return 0
+
+        best_value = None
+        
+        for move in moves:
+            next_state = state.next_state(move)
+            value = MinMax.minmax(next_state)
+                
+            if state.current_player == 0: # MAX
+                if best_value is None or value > best_value:
+                    best_value = value
+
+            else: #MIN
+                if best_value is None or value < best_value:
+                    best_value = value
+
+        return best_value
 
 
 class AlphaBeta(AI):
